0bcffc670270afcf16ad654a9612111856773605
opilane <opilane@HP-CZC6297BB7>
2020-09-23 14:54:41 +0300

    Add disain Tic Tac Toe

---
+++ gridApp/gridApp/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace gridApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            BoxView box;
+            Grid grid = new Grid();
+
+            for (int i = 0; i < 5; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition{ Height = new GridLength(1, GridUnitType.Star) });
+                grid.ColumnDefinitions.Add(new ColumnDefinition{ Width = new GridLength(1, GridUnitType.Star) });
+            }
+                /*{
+                    RowDefinitions =
+                    {
+                        new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                        new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                        new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                        new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                        new RowDefinition { Height = new GridLength(1, GridUnitType.Star) }
+                    },
+                    ColumnDefinitions =
+                    {
+                        new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
+                        new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
+                        new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
+                        new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
+                        new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) }
+                    }
+                };*/
+            for(int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    box = new BoxView { Color = Color.FromRgb(200, 100, 50) };
+                    grid.Children.Add(box, i, j);
+                    var tap = new TapGestureRecognizer();
+                    tap.Tapped += Tap_Tapped;
+                    box.GestureRecognizers.Add(tap);
+                }
+            }
+            Content = grid;
+        }
+
+        private void Tap_Tapped(object sender, EventArgs e)
+        {
+            BoxView box = sender as BoxView;
+            if (box.Color == Color.Black)
+            {
+                box.Color = Color.LightBlue;
+            }
+            else if (box.Color == Color.FromRgb(200, 100, 50))
+            {
+                box.Color = Color.Black;
+            }
+            else if(box.Color == Color.LightBlue)
+            {
+                box.Color = Color.FromRgb(200, 100, 50);
+            }
+            
+        }
+    }
+}
---
+++ gridApp/gridApp/TicTacToe.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace gridApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TicTacToe : ContentPage
+    {
+        public TicTacToe()
+        {
+            BoxView box;
+            Grid grid = new Grid();
+
+            for (int i = 0; i < 3; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(100, GridUnitType.Star) });
+                grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) });
+            }
+
+            AbsoluteLayout absolute = new AbsoluteLayout();
+
+            Button newGame = new Button { BackgroundColor = Color.Black, 
+                TextColor = Color.White, 
+                Text = "New game" };
+            AbsoluteLayout.SetLayoutBounds(newGame, new Rectangle(0.5, 0.02, 175, 50));
+            AbsoluteLayout.SetLayoutFlags(newGame, AbsoluteLayoutFlags.PositionProportional);
+            absolute.Children.Add(newGame);
+
+            Button XO = new Button
+            {
+                BackgroundColor = Color.Black,
+                TextColor = Color.White,
+                Text = "X or O"
+            };
+            AbsoluteLayout.SetLayoutBounds(XO, new Rectangle(0.1, 0.5, 100, 50));
+            AbsoluteLayout.SetLayoutFlags(XO, AbsoluteLayoutFlags.PositionProportional);
+            absolute.Children.Add(XO);
+
+            StackLayout stack = new StackLayout()
+            {
+                Children = {grid, absolute}
+            };
+            Content = stack;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    box = new BoxView { Color = Color.FromRgb(200, 100, 50) };
+                    grid.Children.Add(box, i, j);
+                    var tap = new TapGestureRecognizer();
+                    tap.Tapped += Tap_Tapped; ;
+                    box.GestureRecognizers.Add(tap);
+                }
+            }
+        }
+
+        private void Tap_Tapped(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
